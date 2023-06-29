929d2190e6828b9ecad09aaf3e7c978fda23504c
Shujie Ma <shujie1st@gmail.com>
2023-06-28 20:22:27 -0400

    Setup and complete Exercise 1

--- setup.rb
+++ setup.rb
@@ -10,8 +10,8 @@ puts 'Establishing connection to database ...'
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   database: 'ar_exercises',
-  username: 'development',
-  password: 'development',
+  username: 'shujie1st',
+  password: 'labber',
   host: 'localhost',
   port: 5432,
   pool: 5,
